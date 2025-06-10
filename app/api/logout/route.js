8c0aeb2540d260f28e1fc4cdeb263c0d4477c2f8
Muhammad Adil Farooq <muhammadadilfarooq2006@gmail.com>
2025-06-10 12:01:00 +0500

    logout updates

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { redirect } from "next/dist/server/api-utils";
 
 export async function POST() {
     cookies().delete("session");
-    return redirect('/');
     return NextResponse.json({ message: "Logged out successfully" });
 }
